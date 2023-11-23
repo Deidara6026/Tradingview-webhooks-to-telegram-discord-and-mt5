7b042946520d7403546c216cf5d2e9f35249d1c9
deidara6026 <deidarathedev24@gmail.com>
2023-11-23 22:50:43 +0000

    Haven't made migrations yet because of CustomUser

--- signal_api/models.py
+++ signal_api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
 
 # Create your models here.
+class TelegramAlert(models.Model):
+    channel_invite_link = models.CharField(25)
+    message_format = models.CharField(500)
+    
+
+
+class Signal(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    telegram = models.OneToOneField(TelegramAlert, on_delete=models.CASCADE, null=True, blank=True)
+    telegram_enabled = models.BooleanField()
+    parse = models.BooleanField()
+    
--- signal_api/views.py
+++ signal_api/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from models import Signal
 
-# Create your views here.
+def signal_api_endpoint(request, _id):
+    signal = Signal.objects.get(id=_id)
+    if request.method == 'POST':
+        data = request.POST
+        tradingview_message = data.get('message')
+        if signal.telegram_enabled:
+            if signal.parse:
+                tradingview_message = parse(tradingview_message, signal.telegram)
+            telegram.send(tradingview_message, signal.telegram)
+        # Forward the message to a telegram channel
+        # Your code to forward the message to a telegram channel goes here
+        return JsonResponse({'status': 'success'})
+    else:
+        return JsonResponse({'error': 'Invalid request method'}, status=405)
