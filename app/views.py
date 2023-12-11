d115c9c19f6761e2d11a30132417017d15f34ff0
deidara6026 <deidarathedev24@gmail.com>
2023-12-11 21:33:17 +0000

    Figuring out payments

--- app/views.py
+++ app/views.py
@@ -5,6 +5,9 @@ from django.core import serializers
 from .forms import *
 from signal_api.models import *
 from django.urls import reverse
+import requests
+from django.conf import settings
+
 # Create your views here.
 User = get_user_model()
 
@@ -13,30 +16,52 @@ User = get_user_model()
 def dashboard(request):
     context = {
         # "signalform":SignalForm(),
-
-        }
+    }
     return render(request, "app/dashboard.html", context)
+
+
+def create_checkout(user_id: str):
+    api_url = "https://api.lemonsqueezy.com/v1/checkouts"
+    lemon_settings = settings.LEMONSQUEEZY
+    headers = {
+        "Accept": "application/vnd.api+json",
+        "Content-Type": "application/vnd.api+json",
+        "Authorization": f"Bearer {lemon_settings.api_key}",
+    }
+    data = {
+        {
+            "type": "checkouts",
+            "attributes": {
+                "checkout_data": {"custom": {"user_id": str(user_id)}},
+                "preview": true,
+            },
+            "relationships": {
+                "store": {"data": {"type": "stores", "id": "58249"}},
+                "variant": {"data":{"type":"variants", "id", "145362"}}
+            },
+        }
+    }
 
 
 @login_required
 def submit_alert(request):
     if request.method == "POST":
-        form = 1#SignalForm(request.POST)
+        form = 1  # SignalForm(request.POST)
         if form.is_valid:
             form = form.save(commit=False)
             form.user = request.user
             form.save()
             response = {
-                "name":form.name,
-                "link":f"{reverse('signal_api_endpoint')}/{form.id}",
-                "id": form.id
+                "name": form.name,
+                "link": f"{reverse('signal_api_endpoint')}/{form.id}",
+                "id": form.id,
             }
             return JsonResponse(response)
 
 
 @login_required
 def submit_telegram_link(request, pk):
-    if request.method == 'POST':
+    if request.method == "POST":
         data = request.POST
         form = Telegram_Link_Form(data)
         if form.is_valid:
@@ -48,9 +73,10 @@ def submit_telegram_link(request, pk):
     else:
         return HttpResponse("Only POST requests are allowed")
 
+
 @login_required
 def submit_mt5_link(request, pk):
-    if request.method == 'POST':
+    if request.method == "POST":
         data = request.POST
         form = MT5_Link_Form(data)
         if form.is_valid:
@@ -62,9 +88,10 @@ def submit_mt5_link(request, pk):
     else:
         return HttpResponse("Only POST requests are allowed")
 
+
 @login_required
 def submit_discord_link(request, pk):
-    if request.method == 'POST':
+    if request.method == "POST":
         data = request.POST
         form = Discord_Link_Form(data)
         if form.is_valid:
