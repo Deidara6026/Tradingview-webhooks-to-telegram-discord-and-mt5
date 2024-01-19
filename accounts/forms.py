0077dd6a93ae849aa10d6be8a98daed20e285522
deidara6026 <deidarathedev24@gmail.com>
2024-01-19 10:38:39 +0000

    auth stuff

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,13 +5,23 @@ from django import forms
 from .models import CustomUser
 
 class SignUpForm(UserCreationForm):
+    password1 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(attrs={"class":"form-control", 'id':"password1", "placeholder":"Enter Password"}),
+    )
+    password2 = forms.CharField(
+        label="Confirm password",
+        widget=forms.PasswordInput(attrs={"class":"form-control", 'id':"password2", "placeholder":"Confirm Password"}),
+    )
+
+    
     class Meta:
         model = CustomUser
         fields = ("username", "email", "phone_no")
         widgets = {
-            "username": TextInput(attrs={"class":"form-control"}),
-            "email": TextInput(attrs={"class":"form-control"}),
-            "phone_no": TextInput(attrs={"class":"form-control"}),
+            "username": TextInput(attrs={"class":"form-control", 'id':"userName", "placeholder":"Username"}),
+            "email": TextInput(attrs={"class":"form-control", 'id':"email", "placeholder":"Email"}),
+            "phone_no": TextInput(attrs={"class":"form-control", 'id':"phoneNumber", "placeholder":"Phone Number"}),
         }
 
 class LogInForm(forms.Form):
