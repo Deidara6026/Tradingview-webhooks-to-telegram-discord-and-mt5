771b62d0c844034f93b1f7de5f7ef8c677e62409
Deidara_the_dev <74209287+Deidara6026@users.noreply.github.com>
2024-06-21 22:38:59 +0000

    added toggle visibility to signin page

--- accounts/forms.py
+++ accounts/forms.py
@@ -26,4 +26,4 @@ class SignUpForm(UserCreationForm):
 
 class LogInForm(forms.Form):
     email = forms.EmailField(widget=TextInput(attrs={"class":"input", "id":"floatingInput", "placeholder":"name@example.com"}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={"class":"input", "id":"Password", "placeholder":"Password"}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={"class":"input", "id":"Password", "placeholder":"Password","style":"width:90%;"}))
